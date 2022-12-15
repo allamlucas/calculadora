289989b2576bd81414e1f75c45cec110c9c715df
Lucas <lucas@vstudios3d.local>
2022-12-15 11:51:58 -0300

    corrección de errores

--- src/App.js
+++ src/App.js
@@ -8,60 +8,92 @@ import { evaluate } from 'mathjs';
 
 function App() {
 
-const [input, setInput] = useState ('');
+  const [input, setInput] = useState('');
 
-const agregarInput = val =>{
-  setInput(input + val);
-};
+  const agregarInput = val => {
+    console.log(input.length)
+    console.log(val)
+//     if ((input.length === 0) && 
+//         (val === '+' ||
+//         val === '-' ||
+//         val === '*' ||
+//         val === '/'
+//     )){
+// alert('pone un numero primero')
+//       }
+    if (
+      ((input.length === 0) && 
+         (val === '+' ||
+         val === '-' ||
+         val === '*' ||
+         val === '/'
+     )) ||
+      ((
+      val === '+' ||
+      val === '-' ||
+      val === '*' ||
+      val === '/') &&
+      (input[input.length - 1] === '+' ||
+      input[input.length - 1] === '-' ||
+      input[input.length - 1] === '*' ||
+      input[input.length - 1] === '/'))) {
+     
+        alert('no se puede')
 
-const calcularResultado = () =>{
-  if (input){
-  setInput(evaluate(input));
-  }else
-  alert('Por favor ingrese algun número')
-}
+      }
+    else {
+      setInput(input + val);
+    }
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else
+      alert('Por favor ingrese algun número')
+  }
 
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
-        <img 
-        src= {freeCodeCampLogo}
-        className= 'freecodecamp-logo'
-        alt='Logo de Freecodecamp'
+        <img
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de Freecodecamp'
         />
       </div>
-      <div className= 'contenedor-calculadora'>
-       <Pantalla input={input}/>
-          <div className='fila'>
-            <Boton manejarClick= {agregarInput}>1</Boton>
-            <Boton manejarClick= {agregarInput}>2</Boton>
-            <Boton manejarClick= {agregarInput}>3</Boton>
-            <Boton manejarClick= {agregarInput}>+</Boton>
-          </div>
-          <div className='fila'>
-          <Boton manejarClick= {agregarInput}>4</Boton>
-          <Boton manejarClick= {agregarInput}>5</Boton>
-          <Boton manejarClick= {agregarInput}>6</Boton>
-          <Boton manejarClick= {agregarInput}>-</Boton>
-          </div>
-          <div className='fila'>
-          <Boton manejarClick= {agregarInput}>7</Boton>
-          <Boton manejarClick= {agregarInput}>8</Boton>
-          <Boton manejarClick= {agregarInput}>9</Boton>
-          <Boton manejarClick= {agregarInput}>*</Boton>
-          </div>
-          <div className='fila'>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
           <BotonClear manejarClear={() => setInput('')}>Clear</BotonClear>
-          <Boton manejarClick= {agregarInput}>0</Boton>
-          <Boton manejarClick= {agregarInput}>.</Boton>
-          <Boton manejarClick= {agregarInput}>/</Boton>
-          </div>
-          <div className='fila'>
-           <Boton manejarClick={calcularResultado}>
-              =
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={calcularResultado}>
+            =
           </Boton>
-          </div>
-     </div>
+        </div>
+      </div>
     </div>
   );
 }
